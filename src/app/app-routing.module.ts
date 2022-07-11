4285f537fab8d7a887275a60cfb546ab1a10d299
Tobias Selva <tobiasselva1@gmail.com>
2022-07-11 17:29:42 -0300

    fixxing the app module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { SearchPlayerComponent } from './components/search-player/search-player.component';
 import { UserTeamComponent } from './components/user-team/user-team.component';
-import { ContactComponent } from './components/contact/contact.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'search', component: SearchPlayerComponent },
   { path: 'myteam', component: UserTeamComponent },
-  { path: 'contact', component: ContactComponent },
 ];
 
 @NgModule({
